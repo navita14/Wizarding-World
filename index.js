43628d4992e0bf2b672db634fc484af9d213696b
Navita Ramasar <navitaramasar14@gmail.com>
2023-09-05 15:58:25 -0400

    part finished

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const studentGender = document.getElementById("student-gender");
 const studentHouse = document.getElementById("student-house");
 const studentDateOfBirth = document.getElementById("student-dateofbirth");
 const pics = document.querySelector("#student-pic")
+const form = document.querySelector("#new-form")
+
+const newName = document.querySelector("#name")
+const newGender = document.querySelector("#gender")
+const newHouse = document.querySelector("#house")
+const newDob = document.querySelector("#dob")
+const newImage = document.querySelector("#image-input")
 
 fetch("http://localhost:3000/characters")
 .then(res => res.json())
@@ -18,7 +25,6 @@ fetch("http://localhost:3000/characters")
       studentImage.addEventListener("click", e => {
         addStudents(student)
       });
-
     });
     addStudents(data[0])
   });
@@ -30,5 +36,27 @@ function addStudents(student){
     studentGender.textContent = student.gender;
     studentHouse.textContent = student.house;
     studentDateOfBirth.textContent = student.dateOfBirth;
-
 }
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const imgTwo = document.createElement("img");
+    imgTwo.src = newImage.value;
+    imgTwo.id ="imgTwo"
+    galleryDiv.append(imgTwo);
+    
+    imgTwo.addEventListener("click", e => {
+      addStudents(newStudent)
+    });
+
+    const newStudent = {};
+        newStudent["name"] = newName.value;
+        newStudent["gender"] = newGender.value;
+        newStudent["house"] = newHouse.value;
+        newStudent["dateOfBirth"] = newDob.value;
+        newStudent["image"] = newImage.value;
+
+    addStudents(newStudent)
+    
+})
