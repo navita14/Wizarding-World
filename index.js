e78e1b3ef1bda4686d72d4a5dc4cd1bc50f23c8e
Navita Ramasar <navitaramasar14@gmail.com>
2023-09-05 13:50:01 -0400

    new json

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-fetch("")
+// https://hp-api.onrender.com/api/characters/students
 
+
+
+fetch("https://hp-api.onrender.com/api/characters/students")
+.then(res => res.json())
+.then(data => {
+    data.forEach(student => {
+
+          const studentDetailsDiv = document.createElement("div"); 
+
+    })
+})
+
+function addStudents(student){
+
+
+}
