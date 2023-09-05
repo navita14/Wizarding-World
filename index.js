a6af2aec02861338b773827cb322fac435008d2c
Navita Ramasar <navitaramasar14@gmail.com>
2023-09-05 15:06:23 -0400

    more updates form

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-// https://hp-api.onrender.com/api/characters/students
+const galleryDiv = document.getElementById("student-gallery");
+const detailsContainer = document.getElementById("student-details-container");
+const studentName = document.getElementById("student-name");
+const studentGender = document.getElementById("student-gender");
+const studentHouse = document.getElementById("student-house");
+const studentDateOfBirth = document.getElementById("student-dateofbirth");
+const pics = document.querySelector("#student-pic")
 
-
-
-fetch("https://hp-api.onrender.com/api/characters/students")
+fetch("http://localhost:3000/characters")
 .then(res => res.json())
 .then(data => {
-    data.forEach(student => {
+    data.forEach((student) => {
+      const studentImage = document.createElement("img");
+      studentImage.src = student.image;
+      studentImage.id ="sImage"
+      galleryDiv.appendChild(studentImage);
+      
+      studentImage.addEventListener("click", e => {
+        addStudents(student)
+      });
 
-          const studentDetailsDiv = document.createElement("div"); 
+    });
+    addStudents(data[0])
+  });
 
-    })
-})
 
 function addStudents(student){
-
+    pics.src = student.image;
+    studentName.textContent = student.name;
+    studentGender.textContent = student.gender;
+    studentHouse.textContent = student.house;
+    studentDateOfBirth.textContent = student.dateOfBirth;
 
 }
